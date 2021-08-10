9f8b0a3ab9f3d100ef4067ad5ba7cc7f52dcffaf
Harry Michael <ando.lalaina1103@gmail.com>
2021-08-10 09:23:35 +0300

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import Post from './components/Post'
-import Comment from './components/Comment'
+import Post from './components/Post';
+import Comment from './components/Comment';
+import Header from './components/Header';
 
-export default function ButtonAppBar() {
+import { AppBar, Button, createStyles, makeStyles, Paper, Theme, Toolbar, Typography, Tabs, Tab, Grid } from '@material-ui/core';
+import { AccountCircle, Menu } from '@material-ui/icons';
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
+}));
+
+export default function App() {
+  const classes = useStyles();
   return (
     <div>
+      <Header/>
       <Post></Post>
       <Post>
         <Comment/>
---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { AppBar, Button, createStyles, makeStyles, Paper, Theme, Toolbar, Typography, Tabs, Tab, Grid } from '@material-ui/core';
+import { AccountCircle, Menu } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme: Theme) => createStyles({
+  btnActive: {
+    color: "red"
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  return (
+    <header>
+      <AppBar position="static">
+        <Toolbar className="toolBar">
+          <Typography variant="h6" className={classes.title}>
+            E-pandemic-link
+          </Typography>
+          <Button variant="contained" color="secondary" startIcon={<AccountCircle/>}>Se connecter</Button>
+        </Toolbar>
+      </AppBar>
+      <Paper>
+        <Grid container>
+        <Button className={classes.btnActive}>
+          Accueil
+        </Button>
+        <Button>
+          Santé
+        </Button>
+        <Button>
+          Sport
+        </Button>
+        <Button>
+          Guide de Survie
+        </Button>
+        </Grid>
+      </Paper>
+    </header>
+  );
+}
